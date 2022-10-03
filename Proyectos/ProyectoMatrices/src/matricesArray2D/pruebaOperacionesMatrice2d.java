5c9a9c3e33721e5c5b0152a33d5d1a69bf6e8482
Roberto Méndez Méndez <91458910+RobertoMendezM@users.noreply.github.com>
2022-10-03 07:50:51 -0500

    Add files via upload

---
+++ Proyectos/ProyectoMatrices/src/matricesArray2D/pruebaOperacionesMatrice2d.java
@@ -0,0 +1,18 @@
+/**
+ * Curso: Programación
+ *
+ * Temas: Arreglos Bidimensionales aplicados
+ *          para representar operaciones con matrices
+ *
+ * @author Roberto Méndez Méndez
+ * @version 1/Oct/22
+ *
+ *  Actividad para el alumno:
+ *    Probar los métodos de OperacionesMAtrices2D y
+ *    MAtrizArreglo2D
+ */
+
+package matricesArray2D;
+
+public class pruebaOperacionesMatrice2d {
+}
